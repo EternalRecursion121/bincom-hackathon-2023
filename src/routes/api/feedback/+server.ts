e4ff975960e509f67507b0a70a2e25f1296593ac
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-06-03 14:50:19 +0100

    test?

--- src/routes/api/feedback/+server.ts
+++ src/routes/api/feedback/+server.ts
@@ -1,3 +1,9 @@
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = {
+    runtime: 'nodejs18.x'
+};
+
 import { OPENAI_API_KEY } from "$env/static/private";
 
 const { Configuration, OpenAIApi } = require("openai");
